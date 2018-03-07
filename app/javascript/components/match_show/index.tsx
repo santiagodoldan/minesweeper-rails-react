87e6a543809fce0bdc546b1cb8c0cf96bbf5e79c
Santiago Doldan <santiagodolte@gmail.com>
2018-03-07 11:56:59 -0300

    Draw match grid

--- app/javascript/components/match_show/index.tsx
+++ app/javascript/components/match_show/index.tsx
@@ -1,6 +1,33 @@
 import * as React from "react"
+import { RouteComponentProps, withRouter } from "react-router-dom"
+import { Match } from "../../models/match"
+import { DataService } from "../../services/data_service"
+import { inject } from "../../utils/ioc"
 
-export class MatchShow extends React.Component {
+interface IMatchShowParams {
+  id: number
+}
+
+interface IMatchShowState {
+  match: Match
+}
+
+class MatchShowComponent extends React.Component<RouteComponentProps<IMatchShowParams>, IMatchShowState> {
+
+  @inject(DataService)
+  public dataService: DataService
+
+  public constructor(props: RouteComponentProps<IMatchShowParams>) {
+    super(props)
+
+    this.state = { match: undefined }
+  }
+
+  public async componentDidMount() {
+    const match = await this.dataService.getMatch(this.props.match.params.id)
+
+    this.setState({ match })
+  }
 
   public render() {
     return (
@@ -10,25 +37,24 @@ export class MatchShow extends React.Component {
         <div className="mx-auto mt-5 w-50">
           <table className="table table-bordered table-sm">
             <tbody>
-              <tr>
-                <td className="w-25 text-center">
-                  <i className="fa fa-bomb"></i>
-                </td>
-                <td className="w-25 text-center">
-                  <i className="fa fa-flag-checkered"></i>
-                </td>
-                <td className="w-25 text-center"></td>
-              </tr>
-              <tr>
-                <td className="w-25 text-center">&nbsp;</td>
-                <td className="w-25 text-center"></td>
-                <td className="w-25 text-center"></td>
-              </tr>
-              <tr>
-                <td className="w-25 text-center">&nbsp;</td>
-                <td className="w-25 text-center"></td>
-                <td className="w-25 text-center"></td>
-              </tr>
+              {
+                this.state.match &&
+                  Array.apply(null, { length: this.state.match.rows }).map((_: any, row: number) => {
+                    return (
+                      <tr key={ row }>
+                        {
+                          Array.apply(null, { length: this.state.match.columns }).map((_: any, col: number) => {
+                            return (
+                              <td key={ `${row}:${col}` }>
+                                &nbsp;
+                              </td>
+                            )
+                          })
+                        }
+                      </tr>
+                    )
+                  })
+              }
             </tbody>
           </table>
         </div>
@@ -37,3 +63,5 @@ export class MatchShow extends React.Component {
   }
 
 }
+
+export const MatchShow = withRouter(MatchShowComponent)
--- app/javascript/services/data_service.ts
+++ app/javascript/services/data_service.ts
@@ -12,4 +12,8 @@ export class DataService {
   public async getMatches(): Promise<Match[]> {
     return (this.matches = [{ id: 1, name: "Test", rows: 10, columns: 10, mines: [], flags: []} as Match])
   }
+
+  public async getMatch(id: number): Promise<Match> {
+    return { id: 1, name: "Test", rows: 10, columns: 20, mines: [], flags: []} as Match
+  }
 }
